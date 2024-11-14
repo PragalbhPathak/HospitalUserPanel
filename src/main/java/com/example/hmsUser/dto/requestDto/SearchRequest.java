c22ef983cbbe5a953eff34df3b8d4bd53e925d4d
PragalbhPathak <pragalbhagra5@gmail.com>
2024-11-14 12:37:13 +0530

    Add SearchRequest.java file

---
+++ src/main/java/com/example/hmsUser/dto/requestDto/SearchRequest.java
@@ -0,0 +1,13 @@
+package com.example.hmsUser.dto.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//For general Search DTO
+public class SearchRequest {
+    private Long id;
+}
